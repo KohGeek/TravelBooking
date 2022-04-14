53d22a8fbce53027a1ac2b7725e8ba5f1a63d45b
AdrialYeoh <86753737+AdrialYeoh@users.noreply.github.com>
2022-04-14 19:56:49 +0500

    Add Profile

--- travel/domain/ProfileController.java
+++ travel/domain/ProfileController.java
@@ -1,63 +1,84 @@
-package travel.domain;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testingproject;
 
 /**
  * 
  */
 public class ProfileController {
 
+    /**
+     * Default constructor
+     */
+    public ProfileController() {
+    }
+
+    /**
+     * 
+     */
     private IUserList userLists;
+
 
     /**
      * @param userDataFile
      */
-    public ProfileController(String userDataFile) {
-        this.userLists = new UserLists(userDataFile);
+    public void ProfileController(String userDataFile) {
+        // TODO implement here
     }
 
     /**
-     * @param username
-     * @param password
+     * @param username 
+     * @param password 
      * @return
      */
     public User login(String username, String password) {
-        return userLists.login(username, password);
+        // TODO implement here
+        return null;
     }
 
     /**
-     * @param username
-     * @param password
-     * @param email
-     * @param name
-     * @param address
-     * @param phonenumber
+     * @param username 
+     * @param password 
+     * @param email 
+     * @param name 
+     * @param address 
+     * @param phonenumber 
      * @return
      */
-    public User createClientProfile(String username, String password, String email, String name, String address,
-            String phonenumber) {
-        Client client = new Client(username, password);
+    public User createClientProfile(String username, String password, String email, String name, String address, String phonenumber) {
+        
+        Client client=new Client();
+        client.User(username, password);
         client.setEmail(email);
         client.setName(name);
         client.setAddress(address);
         client.setPhonenumber(phonenumber);
-        return userLists.createClientProfile(client);
+        return client;
     }
 
     /**
-     * @param username
-     * @param value
-     * @param type
+     * @param username 
+     * @param value 
+     * @param type 
      * @return
      */
-    public void updateClientProfile(String username, String value, int type) {
-        userLists.updateClientProfile(username, value, type);
+    public void updateClientProfile(String username, String phone, String  address) {
+        // TODO implement here
+        Client client=new Client();
+        client.User(username, "");
+        client.setAddress(address);
     }
 
     /**
-     * @param user
+     * @param user 
      * @return
      */
     public void updateUser(User user) {
-        userLists.updateUser(user);
+        // TODO implement here
+        return;
     }
 
 }
