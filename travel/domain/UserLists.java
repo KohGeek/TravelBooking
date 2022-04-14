74c3c7ab6dacc1337dd9c58110346196296cdec6
AdrialYeoh <86753737+AdrialYeoh@users.noreply.github.com>
2022-04-14 20:32:03 +0500

    Create Client And Update User

--- travel/domain/UserLists.java
+++ travel/domain/UserLists.java
@@ -1,4 +1,9 @@
-package travel.domain;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testingproject;
 
 import java.util.List;
 
@@ -6,6 +11,12 @@ import java.util.List;
  * 
  */
 public class UserLists implements IUserList {
+
+    /**
+     * Default constructor
+     */
+    public UserLists() {
+    }
 
     /**
      * 
@@ -17,50 +28,81 @@ public class UserLists implements IUserList {
      */
     private String userDataFile;
 
+
     /**
      * @param userDataFile
      */
-    public UserLists(String userDataFile) {
+    public void UserLists(String userDataFile) {
         // TODO implement here
     }
 
     /**
-     * @param username
-     * @param password
+     * @param username 
+     * @param password 
      * @return
      */
     public User login(String username, String password) {
         // TODO implement here
+        
+        for(User u : this.users)
+        {
+            if(u.getUsername().equals(username)&&u.getPassword().equals(password))
+            {
+                return u;
+            }
+        }
+        
         return null;
     }
 
     /**
-     * @param user
+     * @param user 
      * @return
      */
     public User createClientProfile(User user) {
-        // TODO implement here
-        return null;
+        boolean b=true;
+        for(User u : this.users)
+        {
+            if(u.getUsername().equals(user.getUsername())&&u.getPassword().equals(user.getPassword()))
+            {
+                b=false;
+            }
+        }
+        if(b)
+        {
+            this.users.add(user);
+        }
+        return user;
     }
 
     /**
-     * @param username
-     * @param value
-     * @param type
+     * @param username 
+     * @param value  
+     * @param type 
      * @return
      */
-    public void updateClientProfile(String username, String value, int type) {
+    public void updateClientProfile(String username, String value , int type) {
         // TODO implement here
-        return null;
+        Client c = (Client)users.get(type);
+        c.setAddress(value);
+        
     }
 
     /**
-     * @param user
+     * @param user 
      * @return
      */
     public void updateUser(User user) {
         // TODO implement here
-        return null;
+        boolean b=true;
+        for(User u : this.users)
+        {
+            if(u.getUsername().equals(user.getUsername()))
+            {
+                u=user;
+            }
+        }
+        
     }
 
     /**
@@ -68,7 +110,7 @@ public class UserLists implements IUserList {
      */
     private void saveUserFile() {
         // TODO implement here
-        return null;
+        return;
     }
 
     /**
@@ -76,46 +118,8 @@ public class UserLists implements IUserList {
      */
     private void loadUserFile() {
         // TODO implement here
-        return null;
+        return;
     }
 
-    /**
-     * @param username
-     * @param password
-     * @return
-     */
-    public User login(String username, String password) {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param user
-     * @return
-     */
-    public User createClientProfile(User user) {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param username
-     * @param value
-     * @param type
-     * @return
-     */
-    public void updateClientProfile(String username, String value, int type) {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param user
-     * @return
-     */
-    public void updateUser(User user) {
-        // TODO implement here
-        return null;
-    }
 
 }
